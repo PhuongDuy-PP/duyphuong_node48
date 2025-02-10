ca15edc121f34469dd29127b6402072bbafe75f1
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-02-10 09:13:55 +0700

    Push code session 9 - 2

--- backend-youtube/src/controllers/authController.js
+++ backend-youtube/src/controllers/authController.js
@@ -46,4 +46,48 @@ const register = async (req, res) => {
    }
 };
 
-export { register };
+const login = async (req, res) => {
+   try {
+      // 1 - Nhận dữ liệu: email, pass_word
+      const { email, pass_word } = req.body;
+      console.log({ email, pass_word });
+
+      // 2 - Kiểm tra email có tồn tại hay chưa
+      //    - Nếu chưa tồn tại: trả lỗi "Email chưa tồn tại vui lòng đăng ký để sử dụng"
+      //    - Nếu đã tồn tại: đi tiếp
+      const userExists = await models.users.findOne({
+         where: {
+            email: email,
+         },
+      });
+      if (!userExists) {
+         res.status(400).json({ message: "Email chưa tồn tại vui lòng đăng ký để sử dụng" });
+         return;
+      }
+      console.log({ userExists });
+
+      // 2.1 - (Thêm) kiểm tra tk là đăng nhập FB hay GG
+      //  - TK đăng nhập bằng FB (chưa có pass): Trả Lỗi "Vui lòng đăng nhập bằng FB để cập nhật mật khẩu"
+      if (!userExists.dataValues.pass_word) {
+         res.status(400).json({ message: "Không có mật khẩu, Vui lòng đăng nhập bằng FB để cập nhật mật khẩu" });
+         return;
+      }
+
+      // 3 - Kiểm tra password có hợp lệ hay không
+      const isPassword = bcrypt.compareSync(pass_word, userExists.dataValues.pass_word);
+      if (!isPassword) {
+         res.status(400).json({ message: "Mật khẩu không đúng, vui lòng nhập lại" });
+         return;
+      }
+
+      // 3.1 - Gửi email chào mừng
+
+      // 4 - Trả kết quả thành công
+      res.status(200).json(`Đăng nhập thành công`);
+   } catch (error) {
+      console.log(error);
+      res.status(500).json(`Error ${error}`);
+   }
+};
+
+export { register, login };
--- backend-youtube/src/routes/authRouters.js
+++ backend-youtube/src/routes/authRouters.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
@@ -7,5 +7,6 @@ const authRouter = express.Router();
 authRouter.post("/register", register);
 
 // Đăng nhập
+authRouter.post("/login", login)
 
 export default authRouter;
